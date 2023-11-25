d04c9f53ec645c26205ffce5efefb9934b6ea9f8
RITIK <ritikprofessional111@gmail.com>
2023-11-25 14:27:12 +0530

    practising C

--- Variables_DataTypes/v-dt.c
+++ Variables_DataTypes/v-dt.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
+#include <stdio.h> // pre-processor directive
 
-int main(){
-    printf("hey\n");
+int main(){ // main function
+    // printf("hey\n"); 
     int num = 32;
+
+    // integer data type --->
+
+    int num1 = 23;
+    int num2 = 24;
+    printf("num1 is %d & num2 is %d\n", num1, num2);
+    printf("There sum is %d\n", num1 + num2);
     return 0;
 }
+
+
 
