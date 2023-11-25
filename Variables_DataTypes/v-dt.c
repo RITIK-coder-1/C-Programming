cc9c28da930a48855ade777cf76e751034c9ab7b
RITIK <ritikprofessional111@gmail.com>
2023-11-25 14:35:52 +0530

    practising C

--- Variables_DataTypes/v-dt.c
+++ Variables_DataTypes/v-dt.c
@@ -2,14 +2,24 @@
 
 int main(){ // main function
     // printf("hey\n"); 
-    int num = 32;
+
+    int num; // variable declaration
+    num = 23; // variable assignment
+    // printf("%d \n", num);
+    num = 24; // variable reassignment
+    // printf("%d \n", num);
 
     // integer data type --->
 
     int num1 = 23;
     int num2 = 24;
-    printf("num1 is %d & num2 is %d\n", num1, num2);
-    printf("There sum is %d\n", num1 + num2);
+    // printf("num1 is %d & num2 is %d\n", num1, num2);
+    // printf("There sum is %d\n", num1 + num2);
+
+    // character data type --->
+
+    char a = 'a';
+    // printf("The given character is: %c \n", a);
     return 0;
 }
 
