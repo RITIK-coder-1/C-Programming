b356bc1c49fa508d28956a942f2ac64d6430c068
RITIK <ritikprofessional111@gmail.com>
2023-11-25 14:51:08 +0530

    practising C

---
+++ Practice_Questions/one.c
@@ -0,0 +1,10 @@
+// Calculate Area of a square -------->
+
+#include <stdio.h>
+
+int main(){
+    printf("The Area Of a Square: Side * 4 \nEnter the Side Length: ");
+    int side;
+    scanf("%d", &side);
+    printf("The Area: %d\n", side * 4);
+}
