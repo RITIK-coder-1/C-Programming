948ea35c01046430b183064e05ab42310e6b4a2c
RITIK <ritikprofessional111@gmail.com>
2023-11-25 19:49:00 +0530

    practising C

---
+++ Practice_Questions/four.c
@@ -0,0 +1,11 @@
+// Calculate the cube of a number ------->
+
+#include <stdio.h>
+
+int main(){
+    printf("Enter a number: ");
+    int num;
+    scanf("%d", &num);
+    printf("The Cube of the number is: %d\n", num * num * num);
+    return 0;
+}
