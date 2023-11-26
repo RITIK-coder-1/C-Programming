54ca48db6a4d6b019145666a5459f4d115c0160c
RITIK <ritikprofessional111@gmail.com>
2023-11-26 12:14:01 +0530

    practising C

--- Input_Output/io1.c
+++ Input_Output/io1.c
@@ -4,6 +4,11 @@
 
 int main(){
     int num1 = 23;
-    printf("%d\n", num1);
-    printf("The number is: %d\n", num1);
+    printf("%d\n", num1); // output
+    printf("The number is: %d\n", num1); // output with literal characters
+
+    int num2;
+    printf("Enter the number: ");
+    scanf("%d", &num2); // taking input
+    printf("%d\n", num2); // displaying the input as output
 }
