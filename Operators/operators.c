b87df7cbbdd1526cb66ceaab90f05bf1c25b8971
RITIK <ritikprofessional111@gmail.com>
2023-11-26 19:15:26 +0530

    practising C

--- Operators/operators.c
+++ Operators/operators.c
@@ -21,6 +21,9 @@ int main ()
     printf("%d\n", 12 < 13); // true // 1
     printf("%d\n", 12 >= 13); // false // 0
     printf("%d\n", 12 <= 12); // true // 1
+
+    // equality ---->
     printf("%d\n", 12 == 13); // false // 0
+    printf("%d\n", 12 != 13); // true // 1
     return 0;
 }
