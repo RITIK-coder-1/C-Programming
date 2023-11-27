5a754606b11ccd6e512c156ffd066706647dadcc
RITIK <ritikprofessional111@gmail.com>
2023-11-27 19:37:46 +0530

    practising C

--- functions/functions.c
+++ functions/functions.c
@@ -5,9 +5,28 @@ void add(int num1, int num2){
     printf("%d\n", sum);
 }
 
+void greetings(){
+
+    char nationality;
+    printf("Enter I for Indian, F for French, or Something else: ");
+    scanf("%c", &nationality);
+
+    if (nationality == 'I'){
+        printf("Namaste\n");
+    } else if (nationality == 'F'){
+        printf("Bonjour\n");
+    } else {
+        printf("Hello\n");
+    }
+
+}
+
 int main()
 {
-    add(10,222);
+    // add(10,222);
+
+    // greetings();
+    
     return 0;
 }
 
