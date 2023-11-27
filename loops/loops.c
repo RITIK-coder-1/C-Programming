6e76a60686ac0a0ad0bc909f52e47a41a49a5544
RITIK <ritikprofessional111@gmail.com>
2023-11-27 12:45:37 +0530

    practising C

---
+++ loops/loops.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    // For loop ---->
+
+    for (int i = 10; i <= 20; i++)
+    {
+        // printf("%d\n", i);
+    }
+
+    // While loop --->
+    int num = 0;
+    while (num < 20)
+    {
+        // printf("%d\n", num);
+        num++;
+    }
+        
+    return 0;
+}
