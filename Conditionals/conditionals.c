e1719b11d6c325b4bbeca0d61c70ea7943785b8f
RITIK <ritikprofessional111@gmail.com>
2023-11-27 12:26:10 +0530

    practising C

--- Conditionals/conditionals.c
+++ Conditionals/conditionals.c
@@ -1,15 +1,26 @@
-// if statement ---------->
-
 #include <stdio.h>
 
 int main(){
     int num = 0;
+
+    // else if ---->
     if (num == 0){
         printf("Zero\n");
     } else if (num > 0){
         printf("Positive\n");
     } else {
         printf("Negative\n");
+    }
+
+    // nested if ---->
+    if (num == 0){
+        printf("Zero\n");
+    } else {
+        if (num > 0){
+            // printf("Positive\n");
+        } else {
+            // printf("Negative\n");
+        }
     }
     return 0;
 }
