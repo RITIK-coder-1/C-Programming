7e4b1e0387615914475b13c4b8526cdbe9bab396
RITIK <ritikprofessional111@gmail.com>
2023-11-27 21:07:52 +0530

    practising C

--- Practice_Questions/six.c
+++ Practice_Questions/six.c
@@ -2,20 +2,14 @@
 
 #include <stdio.h>
 
-void greetings(){
-
-    char nationality;
-    printf("Enter I for Indian, F for French, or Something else: ");
-    scanf("%c", &nationality);
-
-    if (nationality == 'I'){
-        printf("Namaste\n");
-    } else if (nationality == 'F'){
-        printf("Bonjour\n");
-    } else {
-        printf("Hello\n");
+void table (){
+    int number;
+    printf("Enter any number to get its first 10 multiples: ");
+    scanf("%d", &number);
+    for(int i = 1; i <= 10; i++){
+        int product = number * i;
+        printf("%d\n", product);
     }
-
 }
 
 int main ()
