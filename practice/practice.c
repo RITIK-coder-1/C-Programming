46c8c53178e5f7f62842334c969ca00bf3749928
RITIK <ritikprofessional111@gmail.com>
2023-12-15 13:14:37 +0530

    practising C

--- practice/practice.c
+++ practice/practice.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main (){
-    printf("ritik\n");
-    printf("%d\n", 2 + 2);
+    // printf("ritik\n");
+    // printf("%d\n", 2 + 2);
+
+    int length, breadth; 
+    printf("Enter the given data: \n");
+    printf("Enter length: ");
+    scanf("%d", &length);
+    printf("Enter breadth: ");
+    scanf("%d", &breadth);
+    printf("The area of the rectangle: %d\n", length*breadth);
     return 0;
 }
