a131f0e7b2258647a68f86751add0bf674e08f0c
RITIK <ritikprofessional111@gmail.com>
2023-12-27 22:17:38 +0530

    practised arrays in C

--- arrays/array.c
+++ arrays/array.c
@@ -4,4 +4,14 @@ int main ()
 {
     int arr1[3] = {1, 2, 3};
     printf("%d %d %d\n", arr1[0], arr1[1], arr1[2]);
+
+    int arr2[2];
+    printf("Enter your first number: ");
+    scanf("%d", &arr2[0]);
+
+    printf("Enter your second number: ");
+    scanf("%d", &arr2[1]);
+    
+    printf("Their sum: %d\n", arr2[0] + arr2[1]);
+    return 0;
 }
